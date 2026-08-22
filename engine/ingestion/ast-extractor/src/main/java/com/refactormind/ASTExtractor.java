ebcf2ad6deebd1c65aa948f819cc12315e858ca0
kushal-soni01 <kushalsoni.ec@gmail.com>
2026-08-22 10:33:08 +0530

    Add: Created AST exttractor to fetch the method and field data from the classes in java file

---
+++ engine/ingestion/ast-extractor/src/main/java/com/refactormind/ASTExtractor.java
@@ -0,0 +1,89 @@
+package main.java.com.refactormind; // Correct package definition
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ASTExtractor {
+
+    static class ClassData {
+        String className;
+        List<FieldData> fields = new ArrayList<>();
+        List<MethodData> methods = new ArrayList<>();
+
+        public ClassData(String className) {
+            this.className = className;
+        }
+    }
+
+    static class FieldData {
+        String fieldName;
+        String fieldType;
+
+        public FieldData(String fieldName, String fieldType) {
+            this.fieldName = fieldName;
+            this.fieldType = fieldType;
+        }
+    }
+
+    static class MethodData {
+        String methodName;
+        String returnType;
+
+        public MethodData(String methodName, String returnType) {
+            this.methodName = methodName;
+            this.returnType = returnType;
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Error: No file path provided."); // Use err to avoid breaking Python JSON parser
+            System.exit(1); // Exit code 1 tells Python the subprocess failed
+        }
+
+        try {
+            CompilationUnit cu = StaticJavaParser.parse(new File(args[0]));
+            List<ClassData> projectClasses = new ArrayList<>();
+
+            for (ClassOrInterfaceDeclaration c : cu.findAll(ClassOrInterfaceDeclaration.class)) {
+
+                ClassData classData = new ClassData(c.getNameAsString());
+
+                for (FieldDeclaration field : c.findAll(FieldDeclaration.class)) {
+                    String name = field.getVariables().get(0).getNameAsString();
+                    String type = field.getElementType().asString();
+
+                    classData.fields.add(new FieldData(name, type));
+                }
+
+                for (MethodDeclaration method : c.findAll(MethodDeclaration.class)) {
+                    String name = method.getNameAsString();
+                    String type = method.getTypeAsString();
+
+                    classData.methods.add(new MethodData(name, type));
+                }
+
+                projectClasses.add(classData);
+            }
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            String jsonOutput = gson.toJson(projectClasses);
+
+            System.out.println(jsonOutput);
+
+        } catch (Exception e) {
+            System.err.println("Failed to parse Java file: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
