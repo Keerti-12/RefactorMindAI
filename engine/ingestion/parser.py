e055f4cc8b550611d87158b89970ee2a5f3e176b
kushal-soni01 <kushalsoni.ec@gmail.com>
2026-08-22 10:30:11 +0530

    Add: Created parser function to extract AST from the java file

---
+++ engine/ingestion/parser.py
@@ -0,0 +1,27 @@
+import os
+import json
+import subprocess
+import logging
+
+logger = logging.getLogger(__name__)
+
+def parse_java_file(file_path: str) -> dict:
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    commands = ['java', '-jar', 'engine/ingestion/ASTExtractor.jar', file_path]
+
+    result = subprocess.run(commands, capture_output=True, text=True)
+    if result.returncode:
+        logger.error(f"AST Extraction failed for {file_path}")
+        logger.error(result.stderr)
+        raise RuntimeError(f"AST Extraction failed for {file_path}")
+
+    json_output = json.loads(result.stdout)
+    logger.info(f"Successfully Parsed {file_path} (AST Size: {len(json_output)})")
+
+    return json_output
+
+if __name__ == '__main__':
+    response = parse_java_file('../../tests/sample_java/OrderProcessor.java')
+    print(response)
